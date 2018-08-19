ad4d9f773b090607cf8fd2f4d06f2b3d5a96bc7e
domlinson <42520750+domlinson@users.noreply.github.com>
2018-08-19 21:18:03 +0100

    Changing queue mechanism

--- Web/queue.php
+++ Web/queue.php
@@ -23,18 +23,55 @@ $myNum = -255;
 if (isset($_SESSION["inQueue"]))
 {
 	// already in the queue, so let them know
-	echo "<center>Number in queue: " . $_SESSION["inQueue"] . ". Wait time approx " . ($_SESSION["inQueue"]*5) . " minutes.</center>";
 	//upon refresh if they are queue num 0 then let them in.
+	$queueNum = 0;
+	$handle = fopen("people.queue", "r");
+	if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+		
+		$newLine = preg_replace('/\s+/', '', $line);
+
+		echo $newLine . "<br />";
+        if($newLine == $_SESSION["myID"])
+		{
+			$queueNum = $queueNum + 1;
+			echo $queueNum;
+			break;
+		}
+    }
+
+    fclose($handle);
+	} else {
+    // error opening the file.
+	} 
+	
+		echo "<center>Number in queue: " . $queueNum . ". Wait time approx " . ($queueNum*5) . " minutes.</center>";
+
 }
 else
 {
-	$queue = file_get_contents('./queue.num', true);
-	$myNum = $queue + 1;
+	
+	$file="people.queue";
+	$linecount = 0;
+	$handle = fopen($file, "r");
+	while(!feof($handle)){
+	  $line = fgets($handle);
+	  $linecount++;
+	}
+
+	fclose($handle);
+	//echo $linecount;
+	
+	//$queue = file_get_contents('./queue.num', true);
+	$myNum = $linecount + 1;
 	// Set session variables
-	$_SESSION["inQueue"] = $myNum;
+	$_SESSION["inQueue"] = $linecount;
+	$_SESSION["myID"] = uniqid();
+
 	//$_SESSION["favanimal"] = "cat";
 	echo "<center>Hold Tight! You are number " . $myNum . " in the queue</center>";
 	file_put_contents('./queue.num', $_SESSION["inQueue"]);
+	file_put_contents('people.queue', $_SESSION["myID"].PHP_EOL , FILE_APPEND | LOCK_EX);
 
 // <= PHP 5
 }
